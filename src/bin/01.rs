13ae6bbcfc8ca5c2db7556ed0af0564cb25033b0
Sebastian Holmin <sebastian.holmin@hotmail.com>
2025-12-01 23:43:42 +0100

    WIP day 1

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,11 +1,52 @@
+use num::{Integer, traits::Euclid};
+
 advent_of_code::solution!(1);
 
 pub fn part_one(input: &str) -> Option<u64> {
-    None
+    let mut dial: i32 = 50;
+    let mut zero_count = 0;
+    for line in input.lines() {
+        let amount: i32 = line[1..].parse().unwrap();
+        if line.chars().next().unwrap() == 'R' {
+            dial += amount;
+        } else {
+            dial -= amount;
+        }
+        dial %= 100;
+        if dial == 0 {
+            zero_count += 1;
+        }
+    }
+    Some(zero_count)
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let mut dial: i32 = 50;
+    let mut zero_count: u64 = 0;
+    for line in input.lines() {
+        let mut change: i32 = line[1..].parse().unwrap();
+        if line.chars().next().unwrap() == 'L' {
+            if dial == 0 {
+                zero_count -= 1;
+            }
+            change *= -1;
+        }
+        dial += dbg!(change);
+        let turns = turns(dbg!(dial));
+        println!("turn: {turns:?}");
+        zero_count += turns;
+        // zero_count += dbg!(dial.div_rem_euclid(&100).0).unsigned_abs() as u64;
+        dial = dial.mod_floor(&100);
+    }
+    Some(zero_count)
+}
+
+fn turns(dial: i32) -> u64 {
+    if dial == 0 {
+        1
+    } else {
+        dial.div_mod_floor(&100).0.abs() as u64
+    }
 }
 
 #[cfg(test)]
@@ -15,12 +56,44 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(3));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(6));
+    }
+
+    #[test]
+    fn test_part_two_b() {
+        let result = part_two(
+            &"L51
+L1",
+        );
+        assert_eq!(result, Some(1));
+    }
+
+    #[test]
+    fn test_part_two_c() {
+        let result = part_two(
+            &"L51
+L200",
+        );
+        assert_eq!(result, Some(3));
+    }
+
+    use rstest::rstest;
+
+    #[rstest]
+    #[case(-5, 1)]
+    #[case(-105, 2)]
+    #[case(-205, 3)]
+    #[case(5, 0)]
+    #[case(105, 1)]
+    #[case(505, 5)]
+    #[case(0, 1)]
+    fn test_neg_quotient(#[case] input: i32, #[case] expected: u64) {
+        assert_eq!(expected, turns(input))
     }
 }

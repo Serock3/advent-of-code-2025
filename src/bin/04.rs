74fb30d50cef9e7a0aa64a1dddcff15d36c5676f
Estrozix <estrozix@gmail.com>
2025-12-04 18:35:53 +0100

    potential solution for b

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -9,7 +9,7 @@ advent_of_code::solution!(4);
 pub fn part_one(input: &str) -> Option<u64> {
     let matrix = parse_char_matrix(input);
 
-    let res = matrix
+    let removable_balls = matrix
         .indexed_iter()
         .filter(|(_, c)| **c == '@')
         .filter(|(pos, _)| {
@@ -18,28 +18,39 @@ pub fn part_one(input: &str) -> Option<u64> {
                 .count();
             println!("{:?}: {}", pos, num_adjacent);
             num_adjacent < 4
-        })
-        .count();
+        });
 
-    // let padding_config = vec![(1, 1), (1, 1)];
-
-    // for window in pad(&matrix, &padding_config, PadMode::Constant(0)).windows((3, 3)) {
-    //     println!("");
-    //     // print_matrix(&window);
-    //     //
-    //     let num_paper = window
-    //         .flatten()
-    //         .iter()
-    //         .enumerate()
-    //         .filter(|(i, el)| **el == '@' && *i % 5 != 0)
-    //         .count();
-    //     println!("{:?}", num_paper);
-    // }
-    Some(res as u64)
+    Some(removable_balls.count() as u64)
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let mut matrix = parse_char_matrix(input);
+
+    let mut removed_balls = 0;
+    loop {
+        let removable_balls = matrix
+            .indexed_iter()
+            .map(|(pos, &char)| (pos, char))
+            .filter(|(_, c)| *c == '@')
+            .filter(|(pos, _)| {
+                let num_adjacent = get_surrounding_positions(*pos, matrix.dim())
+                    .filter(|pos| matrix[(pos.0, pos.1)] == '@')
+                    .count();
+                println!("{:?}: {}", pos, num_adjacent);
+                num_adjacent < 4
+            })
+            .collect::<Vec<_>>();
+        if removable_balls.is_empty() {
+            break;
+        }
+        removed_balls += removable_balls.len();
+        for (ball_pos, prev_char) in removable_balls {
+            println!("Removing '{}' at {:?}", prev_char, ball_pos);
+            matrix[ball_pos] = '.';
+        }
+    }
+
+    Some(removed_balls as u64)
 }
 
 #[cfg(test)]

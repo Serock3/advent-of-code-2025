aad6e5c5ca5f584923327584934a7a9916d2defb
Sebastian Holmin <sebastian.holmin@mullvad.net>
2025-12-04 17:30:36 +0100

    WIP
    Co-authored-by: Estrozix <estrozix@gmail.com>
    Co-authored-by: Sebastian Holmin <sebastian.holmin@mullvad.net>

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,6 +1,9 @@
+use advent_of_code::parse_char_matrix;
+
 advent_of_code::solution!(4);
 
 pub fn part_one(input: &str) -> Option<u64> {
+    let matrix = parse_char_matrix(input);
     None
 }
 
@@ -15,7 +18,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(13));
     }
 
     #[test]

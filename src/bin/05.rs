67b4b079b1ea093b2f51e7aa87fadec4e43c3be0
Sebastian Holmin <sebastian.holmin@hotmail.com>
2025-12-05 08:50:42 +0100

    WIP
    Co-authored-by: Estrozix <estrozix@gmail.com>

---
+++ src/bin/05.rs
@@ -0,0 +1,51 @@
+use std::ops::Range;
+
+use itertools::Itertools;
+
+advent_of_code::solution!(5);
+
+pub fn part_one(input: &str) -> Option<u64> {
+    let (ranges, ingredients) = input.split_once("\n\n").unwrap();
+    let ranges: Vec<Range<usize>> = ranges
+        .lines()
+        .map(|l| {
+            let (start, end) = l.split_once('-').unwrap().into();
+            let start = start.parse::<usize>().expect("Could not parse number");
+            let end = end.parse::<usize>().expect("Could not parse number");
+            Range { start, end }
+        })
+        .collect_vec();
+
+    let ingredients: Vec<usize> = ingredients
+        .lines()
+        .map(|i| i.parse::<usize>().expect("Could not parse number"))
+        .collect();
+
+    let count = ingredients
+        .iter()
+        .filter(|&i| ranges.iter().any(|range| range.contains(i)))
+        .count();
+
+    Some(count as u64)
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(3));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
